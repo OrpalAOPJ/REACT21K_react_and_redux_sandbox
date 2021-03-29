1d252f695d5037adfcf2e040d99f954fb304c220
Alfonso <aortizpalma@gmail.com>
2021-03-29 14:45:45 +0300

    react counter update with most functionalities ready.

--- react_counter/src/App.js
+++ react_counter/src/App.js
@@ -5,16 +5,73 @@ import Main from './Main.js';
 import Footer from './Footer.js';
 import React, {Component} from 'react';
 
-const App = () => {
-  return (
-    <div>
-      {/* <h1>This is H1 in App.js</h1> */}
-      <Header />   
-      <Main />   
-      <Footer />
+// const App = () => {
+//   return (
+//  <div>
+//  <h1>This is H1 in App.js</h1>
+//  <Header />   
+//  <Main />   
+//  <Footer />
 
-    </div>
-  )
+//  </div>
+//  )
+//   )
+// }
+
+class App extends Component {
+  state = {
+    counter : 0,
+  }
+
+  addHandler = () => {
+    console.log("Add");
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
+  removeHandler = () => {
+    console.log("Remove");
+    if(this.state.counter !== 0) {
+      this.setState({
+        counter: this.state.counter - 1,
+      });  
+    }    
+  }
+
+
+
+  resetHandler = () => {
+    console.log("Reset");
+    this.setState({
+      counter: 0,
+    })
+  }
+
+  render () {
+    let circleClass = `${
+      this.state.counter === 0
+      ? ""
+      : this.state.counter % 2 === 0
+      ? "even"
+      : "odd"} circle`;
+
+
+    return (
+      <div>
+        <h1 className={circleClass}>{this.state.counter}</h1>
+        <button onClick={this.addHandler}>Add one</button>
+        <button onClick={this.removeHandler}>Remove one</button>
+        <button onClick={this.resetHandler}>Reset</button>
+        {/* <Header />    */}
+        {/* <Main />    */}
+        {/* <Footer /> */}
+
+      </div>
+    )
+  }
+
+
 }
 
 export default App;
